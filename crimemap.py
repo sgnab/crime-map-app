e30a2117d4d42e7d1be55a34d5c4aebbd3ac38d6
seyed <snaba104@uottawa.ca>
2017-05-02 19:05:43 -0400

    modify crimemap

--- crimemap.py
+++ crimemap.py
@@ -19,7 +19,7 @@ def home():
 @app.route('/add',methods=["GET","POST"])
 def addinput():
     try:
-        data=request.args.get('userinput')
+        data=request.form.get('userinput')
         DB.add_input(data)
 
     except Exception as e:
